9c36c2ade4dcf93ef49efa861d0598794c0242d7
Bonny94 <bonazzigiacomo@yahoo.it>
2020-03-20 15:43:19 +0100

    push

--- wordSimilarity/metrics.py
+++ wordSimilarity/metrics.py
@@ -1,12 +1,11 @@
-import csv
 from nltk.corpus import wordnet as wn
 import math
 
-#calcola la profondità massima di WordNet
+# Calcola la profondità massima di WordNet
 MAX_DEPTH = max(max(len(hyp_path) for hyp_path in ss.hypernym_paths()) for ss in wn.all_synsets())
 
-def wup_similarity(synset_x, synset_y):
 
+def wup_similarity(synset_x, synset_y):
     # Prendiamo tutti i percorsi che vanno dal senso "entity" (root di wordnet) al senso synset_x
     synset_x_path = synset_x.hypernym_paths()
 
@@ -43,18 +42,20 @@ def wup_similarity(synset_x, synset_y):
     except NameError:
         return None
 
-def shortestPath(sense1, sense2):
-	minP = sense1.shortest_path_distance(sense2)
 
-	if(minP is None):
-		return 0
-	else:
-		return 2 * MAX_DEPTH - minP
+def shortestPath(sense1, sense2):
+    minP = sense1.shortest_path_distance(sense2)
+
+    if (minP is None):
+        return 0
+    else:
+        return 2 * MAX_DEPTH - minP
+
 
 def LeacockChodorow(sense1, sense2):
-	minP = sense1.shortest_path_distance(sense2)
+    minP = sense1.shortest_path_distance(sense2)
 
-	if(minP is None):
-		return 0
-	else:
-		return -math.log((minP + 1) / (2 * MAX_DEPTH + 1))
+    if (minP is None):
+        return 0
+    else:
+        return -math.log((minP + 1) / (2 * MAX_DEPTH + 1))
