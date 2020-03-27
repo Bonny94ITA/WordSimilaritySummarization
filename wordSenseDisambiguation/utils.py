53cae98080acb7c28d9656bc08927d1b88f100f7
DarkRaider95 <tommy061295@gmail.com>
2020-03-27 20:07:36 +0100

    added menu

---
+++ wordSenseDisambiguation/utils.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+import random
+import nltk
+from nltk.corpus import semcor
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.corpus.reader.wordnet import Lemma
+from nltk import word_tokenize
+
+stop_words = set(stopwords.words('english'))
+stop_words.add(',')
+stop_words.add('.')
+stop_words.add(';')
+stop_words.add(':')
+stop_words.add('etc')
+stop_words.add("'s")
+stop_words.add("Lt.")
+
+# Lettura file
+def read_file():
+    array = []
+    with open("./asset/sentences.txt", "r") as tsv:
+        for line in tsv:
+            array.append(line)
+    array.pop(0)
+    return array
+
+# Questo metodo viene utilizzato per il file sentences.txt
+# Estrazione delle parole e delle posizioni delle parole tra '**'. Viene estratta anche la parte rimanente della frase
+def extract_word(sentences):
+    word_sent = []
+    for sentence in sentences:
+        item = []
+        word_tokens = word_tokenize(sentence)
+        for i, word in enumerate(word_tokens):
+            if len(word) > 4 and word[0] == '*' and word[1] == '*':
+                item.append(word[2:len(word) - 2])  # Parola tra gli '**' [0]
+                item.append(i)  # La posizione della parola nella frase [1]
+        word_tokens.pop(item[1])
+        item.append(word_tokens)  # Il resto della frase [2]
+        word_sent.append(item)
+    return word_sent
+
+# Eliminazione delle stop word (es. and, at, etc...)
+def delete_stop_words(word_tokens):
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    return filtered_sentence
+
+# Part of speech tagging e lemmatizzazione
+def pos_tagging_and_lemming(word_tokens):
+    word_pos_tag = nltk.pos_tag(word_tokens)  #Il pos tagging e da gestire ancora meglio per il lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    lemming_pos = []
+    for words, pos in word_pos_tag:
+        lemming_pos.append(lemmatizer.lemmatize(words))  # Lemmatizzazione
+        lemming_pos.append(pos)
+    return lemming_pos
+
+# Definizione del contesto
+def get_context(sentence_tokens):
+    return pos_tagging_and_lemming(delete_stop_words(sentence_tokens))
+
+# Ritorna un esempio e la glossa del senso
+def get_examples(sense):
+    example = sense.examples()
+    gloss = sense.definition()
+    if len(example) > 0:
+        return example[0] + gloss
+    else:
+        return gloss
+
+# Estrazione di sinonimi random
+def get_synonym(sense):
+    return random.choice(sense.lemmas()).name()
+
+#ricostruisce la frase sostituendo un sinonimo alla parola originale
+def rebuild_sentence(sense, sentence_tokens, index):
+    synonym = get_synonym(sense)
+    sentence = ""
+    for i, token in enumerate(sentence_tokens):
+        if i < index or i > index:
+            sentence += token + " "
+        else:
+            sentence += synonym + " " + token + " "
+    return sentence
+
+#estrae 50 frasi con i lemmi da semcor
+def semcor_extraction(sentence_number=50):
+    sentences = []
+    extracted = []
+
+    for i in range(0, sentence_number):
+        elem = list(filter(lambda sentence_tree:
+                           isinstance(sentence_tree.label(), Lemma) and
+                           sentence_tree[0].label() == "NN", semcor.tagged_sents(tag='both')[i]))                
+
+        if elem:
+            lemma = select_lemma(elem).label()
+            
+            extracted.append(lemma)
+            sentence = " ".join(semcor.sents()[i])
+            
+            sentences.append(removeWord(sentence, lemma.name()))
+
+    return sentences, extracted
+
+#seleziona un lemma che abbia una parola singola
+def select_lemma(lemmas):    
+    right_lemma = False
+    
+    while(not right_lemma):
+        lemma = random.choice(lemmas)
+        if("_" not in lemma):
+            return lemma
+
+#rimuove la parola scelta dalla frase
+def removeWord(sentence, word):
+    tokens = word_tokenize(sentence)
+    filtered_sentence = [w for w in tokens if w != word]
+    return filtered_sentence
--- wordSenseDisambiguation/wordSenseDisambiguation.py
+++ wordSenseDisambiguation/wordSenseDisambiguation.py
@@ -1,81 +1,7 @@
-import nltk
-import metrics
+import utils
 from nltk import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from nltk.corpus import semcor as sc
 from nltk.corpus import wordnet as wn
 import random
-
-stop_words = set(stopwords.words('english'))
-stop_words.add(',')
-stop_words.add('.')
-stop_words.add(';')
-stop_words.add(':')
-stop_words.add('etc')
-stop_words.add("'s")
-stop_words.add("Lt.")
-#stop_words.add('\'\'')
-
-
-# Lettura file
-def read_file():
-    array = []
-    with open("./asset/sentences.txt", "r") as tsv:
-        for line in tsv:
-            array.append(line)
-    array.pop(0)
-    return array
-
-
-# Estrazione delle parole e delle posizioni delle parole tra '**'. Viene estratta anche la parte rimanente della frase
-def extract_word(sentences):
-    word_sent = []
-    for sentence in sentences:
-        item = []
-        word_tokens = word_tokenize(sentence)
-        for i, word in enumerate(word_tokens):
-            if len(word) > 4 and word[0] == '*' and word[1] == '*':  # 4 è il numero minimo di '*'
-                item.append(word[2:len(word) - 2])  # Parola tra gli '**' [0]
-                item.append(i)  # La posizione della parola nella frase [1]
-        word_tokens.pop(item[1])
-        item.append(word_tokens)  # Il resto della frase [2]
-        word_sent.append(item)
-    return word_sent
-
-
-# Eliminazione delle stop word (es. and, at, etc...)
-def delete_stop_words(word_tokens):
-    filtered_sentence = [w for w in word_tokens if not w in stop_words]
-    return filtered_sentence
-
-
-# Part of speech tagging e lemmatizzazione
-def pos_tagging_and_lemming(word_tokens):
-    word_pos_tag = nltk.pos_tag(word_tokens)  # Il pos tagging è da gestire ancora meglio per il lemmatizer
-
-    lemmatizer = WordNetLemmatizer()
-    lemming_pos = []
-    for words, pos in word_pos_tag:
-        lemming_pos.append(lemmatizer.lemmatize(words))  # Lemmatizzazione
-        lemming_pos.append(pos)
-    return lemming_pos
-
-
-# Definizione del contesto
-def get_context(sentence_tokens):
-    return pos_tagging_and_lemming(delete_stop_words(sentence_tokens))
-
-
-# Ritorna un esempio e la glossa del senso
-def get_examples(sense):
-    example = sense.examples()
-    gloss = sense.definition()
-    if len(example) > 0:
-        return example[0] + gloss
-    else:
-        return gloss
-
 
 def overlap(context1, context2, size):  # valutare se fare intersezione o confronto elemento per elemento
     olp = 0
@@ -96,7 +22,7 @@ def max_overlap(context1, context2):
     len2 = len(context2)
     olp = 0
     if len1 <= len2:
-        olp = overlap_intersection(context1, context2, len1)
+        olp = overlap_intersection(context1, context2, len1) #si può cambiare metodo overlap
     else:
         olp = overlap_intersection(context1, context2, len2)
     return olp
@@ -107,35 +33,19 @@ def Lesk_algorithm(word, sentence_tokens):
     synset = wn.synsets(word)
     best_sense = synset[0]
     max_olp = 0
-    sentence_context = get_context(sentence_tokens)
+    sentence_context = utils.get_context(sentence_tokens)
 
     for sense in synset[1:]:
-        sense_examples = get_examples(sense)
-        sense_context = get_context(word_tokenize(sense_examples))
+        sense_examples = utils.get_examples(sense)
+        sense_context = utils.get_context(word_tokenize(sense_examples))
         olp = max_overlap(sentence_context, sense_context)
         if max_olp < olp:
             max_olp = olp
             best_sense = sense
     return best_sense
 
-
-# Estrazione di sinonimi random
-def get_synonym(sense):
-    return random.choice(sense.lemmas()).name()
-
-
-def rebuild_sentence(sense, sentence_tokens, index):
-    synonym = get_synonym(sense)
-    sentence = ""
-    for i, token in enumerate(sentence_tokens):
-        if i < index or i > index:
-            sentence += token + " "
-        else:
-            sentence += synonym + " " + token + " "
-    return sentence
-
 def compute_accuracy():
-    semcor_sentences, semcor_lemmas = metrics.semcor_extraction(100)
+    semcor_sentences, semcor_lemmas = utils.semcor_extraction(50) # modificare se si vuole un numero diverso di frasi
 
     corrects = 0 
     
@@ -149,19 +59,26 @@ def compute_accuracy():
     
     print("Accuracy: ", corrects/len(semcor_lemmas))
 
-
-def main():
-
-    sentences = read_file()
-    """
-    word_sentences = extract_word(sentences)
+def Lesk_test():
+    sentences = utils.read_file()
+    word_sentences = utils.extract_word(sentences)
     for word_sent in word_sentences:
         best_sense = Lesk_algorithm(word_sent[0], word_sent[2])
-        sent = rebuild_sentence(best_sense, word_sent[2], word_sent[1])
+        sent = utils.rebuild_sentence(best_sense, word_sent[2], word_sent[1])
         print("Sentence: {}\n sense: {} definition: {}\n\n".format(sent, best_sense, best_sense.definition()))
-    """
-    compute_accuracy()
     
+
+def main():
+    choice=1
+
+    while(choice != "0"):
+        print("[0] Esci\n[1] Accuracy Semcor\n[2] Lesk_test")
+        choice=input("Inserisci numero: ")
+
+        if(choice == "1"):
+            compute_accuracy()
+        elif(choice == "2"):
+            Lesk_test()
 
 
 
