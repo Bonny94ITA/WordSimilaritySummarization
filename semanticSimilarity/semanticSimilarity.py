e1908069eb05309f4b5aa49fd4f12ece6eacf5e2
Bonny94 <bonazzigiacomo@yahoo.it>
2020-03-28 12:59:31 +0100

    no message

--- semanticSimilarity/semanticSimilarity.py
+++ semanticSimilarity/semanticSimilarity.py
@@ -1,12 +1,10 @@
 import utils
-import sem_eval_mapper
+# import sem_eval_mapper
 from nltk import word_tokenize
 from nltk.corpus import wordnet as wn
 
 
 def main():
-    # print(sem_eval_mapper.map_2_hundred(2))
-
     array = utils.read_file()
     print(array)
     array = utils.extract_word(array)
