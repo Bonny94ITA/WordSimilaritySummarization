4bf4ba2f349084ec87ee2a97449234239458425d
Bonny94 <bonazzigiacomo@yahoo.it>
2020-03-29 20:08:05 +0200

    terzo esercizio

--- semanticSimilarity/semanticSimilarity.py
+++ semanticSimilarity/semanticSimilarity.py
@@ -1,14 +1,49 @@
 import utils
+import pandas as pd
+import numpy as np
 from nltk import word_tokenize
 from nltk.corpus import wordnet as wn
 
 
+# Calcolo delle correlazioni con Pearson e Spearman
+def compute_correlations(correlations):
+    df = pd.DataFrame(correlations)
+    corrs = [["Pearson", df.corr()]]
+    return corrs
+
+
+# Inizializzazione dizionario (WP = Wu_Palmer, SP = Shortest_Path, LC = Leacock_Chodorow)
+def init_annotation(eval_uno, eval_due):
+    matrix_uno = np.array(eval_uno).transpose()
+    matrix_due = np.array(eval_due).transpose()
+    annotation = {"Annotation_Uno": matrix_uno[2].astype(int), "Annotation_Due": matrix_due[2].astype(int)}
+    return annotation
+
+
+def means(eval_uno, eval_due):
+    return (eval_uno + eval_due) / 2
+
+
+def write_output(eval_uno, eval_due, correlation):
+    with open("./asset/output.txt", "w") as tsv:
+        for uno, due in zip(eval_uno, eval_due):
+            tsv.write(uno[0] + " " + uno[1] + " " + uno[2] + " " + due[2] + " " + str(means(int(uno[2]), int(due[2]))) + "\n")
+        tsv.write(correlation[0][1][5])
+
+
 def main():
-    array = utils.read_file()
-    print(array)
-    array = utils.extract_word(array)
-    print(array)
-    print(len(array))
+    path_uno = "./asset/bonazzi.it.test.data.txt"
+    path_due = "./asset/toscano.it.test.data.txt"
+    reading_uno = utils.read_file(path_uno)
+    reading_due = utils.read_file(path_due)
+    eval_uno = utils.extract_word(reading_uno)
+    eval_due = utils.extract_word(reading_due)
+    annotation = init_annotation(eval_uno, eval_due)
+    correlation = compute_correlations(annotation)
+    print(correlation)
+    write_output(eval_uno, eval_due, correlation)
+
+    # print(means(eval_uno, eval_due))
 
 
 if __name__ == '__main__':
--- semanticSimilarity/utils.py
+++ semanticSimilarity/utils.py
@@ -6,9 +6,9 @@ from nltk import word_tokenize
 
 
 # Lettura file
-def read_file():
+def read_file(path):
     array = []
-    with open("./asset/bonazzi.it.test.data.txt", "r") as tsv:
+    with open(path, "r") as tsv:
         for line in tsv:
             array.append(line)
     return array
@@ -17,8 +17,8 @@ def read_file():
 # Pulizia del file letto
 def extract_word(array):
     words_pair = []
-    for elem in array:
+    for elem in array[200:400]:
         tmp = re.split(r'\t+', elem.rstrip('\t'))
-        tmp[1] = tmp[1].rstrip('\n')
+        tmp[2] = tmp[2].rstrip('\n')
         words_pair.append(tmp)
     return words_pair
