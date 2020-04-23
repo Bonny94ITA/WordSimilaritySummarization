975869a8d3bfb094b0f871a8bf6b407b7cc0acc1
Bonny94 <bonazzigiacomo@yahoo.it>
2020-04-23 18:40:14 +0200

    push

--- nasari/nasari.py
+++ nasari/nasari.py
@@ -1,35 +1,37 @@
 import utils
+import math
 import itertools
 import importlib.util
 from nltk import word_tokenize
-import random
 from nltk import sent_tokenize
 
-spec = importlib.util.spec_from_file_location("wordSenseDisambiguation", "../wordSenseDisambiguation/wordSenseDisambiguation.py")
+spec = importlib.util.spec_from_file_location("wordSenseDisambiguation",
+                                              "../wordSenseDisambiguation/wordSenseDisambiguation.py")
 wsb = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(wsb)
 
-#similarità con weighted overlap
-def weighted_overlap(vect1, vect2):
 
+# similarità con weighted overlap
+def weighted_overlap(vect1, vect2):
     tot = 0.0
     overlap = 0
     for i, elem in enumerate(vect1):
         try:
-            index = vect2.index(elem)+1
+            index = vect2.index(elem) + 1
             overlap += 1
         except:
             index = -1
-        
-        if(index != -1):
-            tot+= (i+1+index)**(-1)
+
+        if (index != -1):
+            tot += (i + 1 + index) ** (-1)
 
     denominatore = 1.0
 
-    for i in range (1, overlap+1):
-        denominatore += (2*i)**(-1)
+    for i in range(1, overlap + 1):
+        denominatore += (2 * i) ** (-1)
 
-    return tot/denominatore
+    return tot / denominatore
+
 
 # calcola la similarità tra i due concetti
 def compute_similarity(bab_id1, bab_id2, nasari):
@@ -38,28 +40,30 @@ def compute_similarity(bab_id1, bab_id2, nasari):
     vect1 = nasari.get(bab_id1)  # Estraiamo il vettore per ogni babel synset id
     vect2 = nasari.get(bab_id2)
 
-    #print(vect1)
-    #print(vect2)
+    # print(vect1)
+    # print(vect2)
 
     if vect1 is not None and vect2 is not None:
         sim = weighted_overlap(vect1["vect"], vect2["vect"])
-    
+
     return sim
 
-#somma la similarità della tupla
-def similarity_tuple(tuple_ids, word_to_synset, nasari):       
+
+# somma la similarità della tupla
+def similarity_tuple(tuple_ids, word_to_synset, nasari):
     sim = 0
 
-    for i, bab_id1 in enumerate(tuple_ids):        
-        for bab_id2 in tuple_ids[i+1:]:
-            sim += compute_similarity(bab_id1, bab_id2, nasari)                    
-    
-    #print(sim)
+    for i, bab_id1 in enumerate(tuple_ids):
+        for bab_id2 in tuple_ids[i + 1:]:
+            sim += compute_similarity(bab_id1, bab_id2, nasari)
+
+            # print(sim)
 
     return sim
 
-#fa l'intersezione tra tutti i vettori per calcolare la similarità
-def similarity_tuple_intersection(tuple_ids, word_to_synset, nasari):       
+
+# fa l'intersezione tra tutti i vettori per calcolare la similarità
+def similarity_tuple_intersection(tuple_ids, word_to_synset, nasari):
     sim = 0
     inter = None
 
@@ -69,68 +73,68 @@ def similarity_tuple_intersection(tuple_ids, word_to_synset, nasari):
         if vect is not None:
             bab_set = set(nasari[bab_id1])
 
-            if(inter is None):
+            if inter is None:
                 inter = bab_set
             else:
                 inter.intersection(bab_set)
         else:
-            inter = None    
-            break                 
-    
-    #print(sim)
-    if(inter != None):
+            inter = None
+            break
+
+            # print(sim)
+    if inter != None:
         sim = len(inter)
 
     return sim
 
-#concateno in una lista i significati per ogni parola del titolo
+
+# concateno in una lista i significati per ogni parola del titolo
 def get_babel_ids(title, word_to_synset):
     babel_ids = []
-    
+
     for word in title:
         babel_ids.append(word_to_synset[word])
 
     return babel_ids
 
-#prende il primo vettore di nasari esistente per i babel synset della parola
+
+# prende il primo vettore di nasari esistente per i babel synset della parola
 def get_vector(word, word_to_synset, nasari):
     babel_ids = word_to_synset[word]
 
     for bab_id in babel_ids:
         vect = nasari.get(bab_id)
-        
-        if(vect is not None):
-            return vect
 
+        if vect is not None:
+            return vect
     return None
 
+
 def clean_context(context):
-    
     clean_context = []
 
-    for c in context:        
-        if c not in clean_context:            
-            clean_context.append(c)
-    
-    return clean_context
+    for c in context:
+        if c['vect'] not in clean_context:
+            clean_context.append(c['vect'])
+    return list(itertools.chain(*clean_context))
 
 
 def get_context(title, word_to_synset, nasari):
     context = []
 
-    for chunk in utils.grouper(title, 6):#il numero cambia la dimensione del contesto da tenere in considerazione
+    for chunk in utils.grouper(title, 6):  # il numero cambia la dimensione del contesto da tenere in considerazione
 
         print("chunk", chunk)
         babel_ids = get_babel_ids(chunk, word_to_synset)
-        
+
         lista_ids = list(itertools.product(*babel_ids))
 
         print("lunghezza combinazioni", len(lista_ids))
         max_sim_tup = 0
         best_tup_ids = lista_ids[0]
 
-        for tuple_ids in lista_ids:# Ci sono due metodi per misurare la similarità            
-            #sim_tup = similarity_tuple_intersection(tuple_ids, word_to_synset, nasari)
+        for tuple_ids in lista_ids:  # Ci sono due metodi per misurare la similarità
+            # sim_tup = similarity_tuple_intersection(tuple_ids, word_to_synset, nasari)
             sim_tup = similarity_tuple(tuple_ids, word_to_synset, nasari)
 
             if sim_tup > max_sim_tup:
@@ -147,76 +151,122 @@ def get_context(title, word_to_synset, nasari):
 
                 if vect is not None:
                     context.append(vect)
-                
-    context = clean_context(context)    
+
+    context = clean_context(context)
     return context
 
-def clean_text(dictionary):
 
+def clean_title(dictionary):
     unified = utils.unify_name(dictionary["Titolo"])
-    dictionary["Titolo"]=utils.delete_stop_words(unified)
-    
-    for i, paragraph in enumerate(dictionary["Paragrafi"]):
-        unified = utils.unify_name(paragraph)
-        dictionary["Paragrafi"][i] = utils.delete_stop_words(unified)
-
+    dictionary["Titolo"] = utils.delete_stop_words(unified)
     return dictionary
 
-def weight_sentence(sent, context):
-    return
 
-def weight_paragraph(paragraph, context):
+def weight_sentence(sent, context, keywords):
+    score = 0
+
+    sent_copy = sent.lower()
+    for phrase in utils.bonus:
+        if phrase in sent_copy:
+            score += 1  # aumento score dato che contiene delle frasi bonus
+
+    sent_token = utils.unify_name(word_tokenize(sent))
+    for word in sent_token:
+        if word in context:
+            score += 1  # aumento score dato che contiene parole del contesto del titolo
+        if word in keywords:
+            score += 2  # aumento score dato che contiene delle keywords
+    return score
+
+
+def weight_paragraph(paragraph, context, keywords):
     sentences = sent_tokenize(paragraph)
-
-    parag_weight = 0.0
     parag = []
+    parag_weight = 0
+    sent_weight = 0
 
-    for sent in sentences:
-        sent_weight+=weight_sentence(sent, context)
-        parag.append([sent, parag_weight])
-        parag_weight+=sent_weight
-
+    for i, sent in enumerate(sentences):
+        sent_weight += weight_sentence(sent, context, keywords)
+        parag.append([sent_weight, sent, i])
+        parag_weight += sent_weight
+    parag.sort(reverse=True)
     return [parag_weight, parag]
-    
 
-def rank_paragraphs(dictionary, context):
 
+def rank_paragraphs(dictionary, context, keywords):
     ranked_parag = []
 
-    for paragh in dictionary["Paragrafi"]:
-        ranked_parag.append(weight_paragraph(paragh, context))
-
+    for i, paragh in enumerate(dictionary["Paragrafi"]):
+        weighted = weight_paragraph(paragh, context, keywords)
+        weighted.append(i)
+        ranked_parag.append(weighted)
+    ranked_parag[0][0] += 2  # aumento score dato che è il primo paragrafo
+    ranked_parag[-1][0] += 2  # aumento score dato che è l'ultimo paragrafo
+    ranked_parag.sort(reverse=True)
+    print(ranked_parag)
     return ranked_parag
 
 
+def compression_ratio(rank_p, ratio):
+    tot = 0
+    for score in rank_p:
+        score[0] = score[0] ** -1
+        tot += score[0]
+    ok = 0
+    for score in rank_p:
+        score[0] = (score[0] / tot) * ratio
+        ok+= score[0]
+    print(ok)
+
+
+def summarize(rank_p, ratio):
+    compression_ratio(rank_p, ratio)
+    tot = tot1 = tot2 = 0
+    for paragraph in rank_p:
+        num_sent = len(paragraph[1])
+        num_sent_del = math.floor(paragraph[0] * num_sent)
+        tot += paragraph[0]*num_sent/0.1
+
+        if num_sent_del != 0:
+            paragraph[1] = paragraph[1][:-num_sent_del]
+        tot1 += num_sent
+        tot2 += num_sent_del
+    print("*****************")
+    print(tot)
+    print(tot1)
+    print(tot2/tot1)
+    print(rank_p)
+
+    return
+
+
 def main():
-    #path = "./asset/Donald-Trump-vs-Barack-Obama-on-Nuclear-Weapons-in-East-Asia.txt"
-    #path = "./asset/People-Arent-Upgrading-Smartphones-as-Quickly-and-That-Is-Bad-for-Apple.txt"
-    path = "./asset/The-Last-Man-on-the-Moon--Eugene-Cernan-gives-a-compelling-account.txt"
+    path = "./asset/Donald-Trump-vs-Barack-Obama-on-Nuclear-Weapons-in-East-Asia.txt"
+    # path = "./asset/People-Arent-Upgrading-Smartphones-as-Quickly-and-That-Is-Bad-for-Apple.txt"
+    # path = "./asset/The-Last-Man-on-the-Moon--Eugene-Cernan-gives-a-compelling-account.txt"
     path_synsets = "./asset/synsets.txt"
     path_nasari = "./asset/dd-nasari.txt"
 
     synsets = utils.read_file_synset(path_synsets)
     word_to_synset = utils.word_to_synset_dict(synsets)
 
-    nasari = utils.read_file_nasari(path_nasari)
+    # nasari = utils.read_file_nasari(path_nasari)
 
     text = utils.read_file(path)
 
     keywords = utils.get_key_words(text)
+    # print(keywords)
 
-    print(keywords)
+    dictionary = utils.paragraph(text)
+    dictionary = clean_title(dictionary)
+    # print(dictionary)
 
-    dictionary = utils.paragraph(text)    
-    dictionary = clean_text(dictionary)
+    # context = get_context(dictionary["Titolo"], word_to_synset, nasari)
+    # print(context)
+    context = []
 
-    #print(dictionary)
-    #context = get_context(dictionary["Titolo"], word_to_synset, nasari)
-
-
-    #rank_paragraphs(dictionary, context)
-
-    print(context)
+    rank_p = rank_paragraphs(dictionary, context, keywords)
+    summary = summarize(rank_p, ratio=0.1)
 
 
 if __name__ == '__main__':
--- nasari/utils.py
+++ nasari/utils.py
@@ -36,171 +36,198 @@ stop_words.add("That")
 stop_words.add("Are")
 stop_words.add("Is")
 
+bonus = ['the main aim', 'the purpose', 'in this report', 'outline', 'our investigation has shown that',
+         'finally', 'certainly', 'completely', 'decisively', 'definitely', 'permanently', 'lastly', 'assuredly',
+         'beyond recall', 'beyond shadow of doubt', 'conclusively', 'for ever', 'for good', 'in conclusion',
+         'inescapably', 'inexorably', 'irrevocably', 'once and for all', 'past regret', 'with conviction',
+         'absolutely', 'assuredly', 'exactly', 'surely', 'clearly', 'beyond any doubt', 'categorically',
+         'doubtlessly', 'explicitly', 'expressly', 'far and away', 'indubitably', 'no ifs ands or buts about it',
+         'positively', 'specifically', 'unmistakably', 'without doubt', 'without fail', 'without question',
+         'the documentary opens', 'also caused', 'statements', 'news', 'lower', 'higher', 'greater', 'most', 'many',
+         'more', 'much', 'better', 'best', 'partially', 'slightly', 'somewhat', 'at best', 'at most', 'bit by bit',
+         'by degrees', 'halfway', 'in a general way', 'in bits and pieces', 'in part', 'in some measure', 'main',
+         'expectations', 'driven by', 'suggested that', 'the decisive factor', 'the big question raised',
+         'there is one thing', 'the final', 'a challenge', 'we are led to imagine', 'brought out', 'important part',
+         'showing', 'the first', 'once again reminds us', 'important', 'principal']
+
 
 # Lettura file
 def read_file(path):
-	array = []
-	with open(path, "r", encoding='utf8') as tsv:
-		for line in tsv:
-			array.append(line)
-	return array[4:]
+    array = []
+    with open(path, "r", encoding='utf8') as tsv:
+        for line in tsv:
+            array.append(line)
+    return array[4:]
 
 
 # Lettura file
 def read_file_synset(path):
-	array = []
-	with open(path, "r") as tsv:
-		for line in tsv:
-			array.append(line)
-	return array
+    array = []
+    with open(path, "r") as tsv:
+        for line in tsv:
+            array.append(line)
+    return array
 
 
 def read_file_nasari(path):
-	sense_to_vector = {}
-	with open(path, encoding='utf8') as tsv:
-		for line in csv.reader(tsv, dialect="excel-tab"):
-			result = [x.strip() for x in line[0].split(';')]  # Prelevo un vettore
+    sense_to_vector = {}
+    with open(path, encoding='utf8') as tsv:
+        for line in csv.reader(tsv, dialect="excel-tab"):
+            result = [x.strip() for x in line[0].split(';')]  # Prelevo un vettore
 
-			# Prelevo il babel synset id
-			sense = result[0]
-			# Prelevo tutte le parole senza '_' (alcuni vettori hanno più parole + qualche refuso) (non considero la stringa vuota)
-			lemma = [x.lower() for x in result[1:] if '_' not in x if x]
-			# Prelevo ogni elemento (x) dalla coda del vettore e separo l'elemento (x) tra il lemma e il peso
-			vettore = [nasari_lemma_peso(*x.split('_')) for x in result[1:] if x if '_' in x]
+            # Prelevo il babel synset id
+            sense = result[0]
+            # Prelevo tutte le parole senza '_' (alcuni vettori hanno più parole + qualche refuso) (non considero la stringa vuota)
+            lemma = [x.lower() for x in result[1:] if '_' not in x if x]
+            # Prelevo ogni elemento (x) dalla coda del vettore e separo l'elemento (x) tra il lemma e il peso
+            vettore = [nasari_lemma_peso(*x.split('_')) for x in result[1:] if x if '_' in x]
 
-			sense_to_vector[sense] = {
-				'lemma': lemma,
-				'vect': vettore
-			}
+            sense_to_vector[sense] = {
+                'lemma': lemma,
+                'vect': vettore
+            }
 
-	return sense_to_vector
+    return sense_to_vector
 
 
 # Inizializzazione dizionario per small nasari per separare lemma e peso
 def nasari_lemma_peso(lemma, peso):
-	return lemma
-	# return {"Lemma": lemma, "Peso": peso}
+    return lemma
+
+
+# return {"Lemma": lemma, "Peso": peso}
 
 
 # Inizializzazione dizionario
 def init_dictionary():
-	dictionary = {"Titolo": "", "Paragrafi": []}
-	return dictionary
+    dictionary = {"Titolo": "", "Paragrafi": []}
+    return dictionary
+
 
 def get_key_words(text):
-	new_text = ""
+    new_text = ""
 
-	for sent in text:
-		new_text+=sent
+    for sent in text:
+        new_text += sent
 
-	return keywords(new_text).split("\n")
+    return keywords(new_text, words=10).split("\n")
+
 
 # Divisione del testo in titolo e paragrafi
 def paragraph(text):
-	dictionary = init_dictionary()
-	dictionary["Titolo"] = word_tokenize(text[0].rstrip('\n'))
-	for elem in text[2:]:
-		if elem != "\n":
-			dictionary["Paragrafi"].append(word_tokenize(elem.rstrip('\n')))
-	return dictionary
+    dictionary = init_dictionary()
+    dictionary["Titolo"] = word_tokenize(text[0].rstrip('\n'))
+    for elem in text[2:]:
+        if elem != "\n":
+            dictionary["Paragrafi"].append(elem.rstrip('\n'))
+    return dictionary
 
-#controllo se la parola è presente nella frase
+
+# controllo se la parola è presente nella frase
 def check_in_sentence(word, sentence):
-	for w in sentence:        
-		if word == w or word in w:
-			return True
+    for w in sentence:
+        if word == w or word in w:
+            return True
 
-	return False
+    return False
 
-#per verificare se una parola è un nome proprio controllo:
+
+# per verificare se una parola è un nome proprio controllo:
 # se la prima lettera è maiuscola 
 # se la parola è un nome
-def check_proper_noun(word):                    #controllo il pos tag della parola sia in minuscolo che maiuscolo
-												#per capire se è un nome, Is ad esempio lo da come nome mentre è un verbo, AT di AT & T non è una preposizione	
-	return word[0].isupper() and nltk.pos_tag([word.lower()])[0][1]=="NN" # and len(wn.synsets(word)) == 0
+def check_proper_noun(word):  # controllo il pos tag della parola sia in minuscolo che maiuscolo
+    # per capire se è un nome, Is ad esempio lo da come nome mentre è un verbo, AT di AT & T non è una preposizione
+    return word[0].isupper() and nltk.pos_tag([word.lower()])[0][1] == "NN"  # and len(wn.synsets(word)) == 0
 
-#Uniamo i nomi propri all'interno di frasi in un'unica parola
+
+# Uniamo i nomi propri all'interno di frasi in un'unica parola
 def unify_name(sentence):
-	sentence_word = []    
-	
-	for i, word in enumerate(sentence):
-		check = check_in_sentence(word, sentence_word)
-				
-		if(check_proper_noun(word) and not check):            
-			w=word
-			for word1 in sentence[i+1:]:
-				if(check_proper_noun(word1)):
-					w+=" "+word1
-				else:
-					break
-			
-			sentence_word.append(w)            
-		else:
-			if(not check):
-				sentence_word.append(word)
+    sentence_word = []
 
-	return sentence_word
+    for i, word in enumerate(sentence):
+        check = check_in_sentence(word, sentence_word)
+
+        if (check_proper_noun(word) and not check):
+            w = word
+            for word1 in sentence[i + 1:]:
+                if (check_proper_noun(word1)):
+                    w += " " + word1
+                else:
+                    break
+
+            sentence_word.append(w)
+        else:
+            if not check:
+                sentence_word.append(word)
+
+    return sentence_word
+
 
 # Eliminazione delle stop word (es. and, at, etc...)
 def delete_stop_words(word_tokens):
-	filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]    
+    filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
 
-	return filtered_sentence
+    return filtered_sentence
 
 
 # Estrazione del senso da babelnet.io
 def get_synset(lemma):
-	service_url = 'http://babelnet.io/v5/getSynsetIds'
-	params = {
-		'lemma': lemma,
-		'searchLang': 'EN',
-		'key': '9f73a72d-0ad4-4f30-bf46-5249377326cc'
-	}
+    service_url = 'http://babelnet.io/v5/getSynsetIds'
+    params = {
+        'lemma': lemma,
+        'searchLang': 'EN',
+        'key': '9f73a72d-0ad4-4f30-bf46-5249377326cc'
+    }
 
-	url = service_url + '?' + urllib.parse.urlencode(params)
-	http = urllib3.PoolManager()
-	response = http.request('GET', url)
-	babel_synset = json.loads(response.data.decode('utf-8'))
+    url = service_url + '?' + urllib.parse.urlencode(params)
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    babel_synset = json.loads(response.data.decode('utf-8'))
 
-	print(lemma)
-	print(response)
-	print(babel_synset)
+    print(lemma)
+    print(response)
+    print(babel_synset)
 
-	return ['BABEL SYNSET NOT FOUND'] if 'message' in babel_synset else babel_synset
+    return ['BABEL SYNSET NOT FOUND'] if 'message' in babel_synset else babel_synset
 
 
 # Dizionario con parola come chiave e babel synset id come valore
 def word_to_synset_dict(babel_synsets):
-	word_2_babel = dict()
+    word_2_babel = dict()
 
-	for bab_syn in babel_synsets:
-		if bab_syn[0] == '#':
-			key = bab_syn[1:-1]
-			word_2_babel[key] = []
-		else:            
-			if(len(word_2_babel[key]) < 15):#	NUMERO DI BABEL SYNSET DA PRENDERE IN CONSIDERAZIONE NEL FILE SYNSETS.txt
-				word_2_babel[key].append(bab_syn[:-1])            
-	return word_2_babel
+    for bab_syn in babel_synsets:
+        if bab_syn[0] == '#':
+            key = bab_syn[1:-1]
+            word_2_babel[key] = []
+        else:
+            if (len(word_2_babel[
+                        key]) < 15):  # NUMERO DI BABEL SYNSET DA PRENDERE IN CONSIDERAZIONE NEL FILE SYNSETS.txt
+                word_2_babel[key].append(bab_syn[:-1])
+    return word_2_babel
 
-#raggruppa dati in chunk di lunghezza fissa
-#se mancano dati prende gli elementi prima fino a formare il chunk di n elementi
-def grouper(iterable, n, fillvalue=None):		
-		# grouper('ABCDEFG', 3) --> ABC DEF GEF
-		resto = len(iterable) % n 
-		
-		if(resto != 0):
-			iterable += iterable[-n:-resto]
 
-		args = [iter(iterable)] * n
+# raggruppa dati in chunk di lunghezza fissa
+# se mancano dati prende gli elementi prima fino a formare il chunk di n elementi
+def grouper(iterable, n, fillvalue=None):
+    # grouper('ABCDEFG', 3) --> ABC DEF GEF
+    resto = len(iterable) % n
 
-		return zip_longest(fillvalue=fillvalue, *args)
+    if (resto != 0):
+        iterable += iterable[-n:-resto]
+
+    args = [iter(iterable)] * n
+
+    return zip_longest(fillvalue=fillvalue, *args)
+
 
 def main():
-	x = [1,2,3,4,5,6,7,8,9]
-	g = grouper(x,7)
-	
-	print(x)
-	for p in g:
-		print(p)
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    g = grouper(x, 7)
+
+    print(x)
+    for p in g:
+        print(p)
+
+
 if __name__ == '__main__':
     main()
